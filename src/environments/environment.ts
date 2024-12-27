e5a5c08fef4975ff0813e0ca806c39bd673a5376
Mohamed Lafir umair <99977258+DevoUmair@users.noreply.github.com>
2024-12-27 20:48:02 +0500

    Enviroment Was Updatd

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
     production: false,
-    // baseUrl: "http://localhost:51865/api"
-     baseUrl: "https://labs.octdaily.com/api/"
+    baseUrl: "http://localhost:51865/api"
   };
   
